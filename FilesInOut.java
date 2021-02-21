bd306a313eee4ab7e516ea57e56fa38a5e428772
Ssourc3 <78283109+Ssourc3@users.noreply.github.com>
2021-02-21 20:20:53 +0000

    Add files via upload

--- FilesInOut.java
+++ FilesInOut.java
@@ -6,7 +6,10 @@ import javax.swing.*;
 import java.lang.Number;
 import java.io.File;
 import java.util.Scanner;
-import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 
 
 /**
@@ -14,44 +17,90 @@ import java.io.File;
  * CSCU9T4 Java strings and files exercise.
  *
  */
-public class FilesInOut {
-
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
-
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-    	try {
-            System.out.print("Enter the file name with extension : ");
-
-            Scanner input = new Scanner(System.in);
-
-            File file = new File(input.nextLine());
-
-            input = new Scanner(file);
+public class FilesInOut 
 
 
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println(line);
-            }
-            input.close();
+	public static void main(String[] args) {
+		// Replace this with statements to set the file name (input) and file name (output).
+		// Initially it will be easier to hardcode suitable file names.
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+		// Set up a new Scanner to read the input file.
+		// Processing line by line would be sensible here.
+		// Initially, echo the text to System.out to check you are reading correctly.
+		// Then add code to modify the text to the output format.
 
+		// Set up a new PrintWriter to write the output file.
+		// Add suitable code into the above processing (because you need to do this line by line also.
+		// That is, read a line, write a line, loop.
 
-    } // main
+		// Finally, add code to read the filenames as arguments from the command line.
 
- // FilesInOut
+		try {
+			File inputFile;
+			File outputFile;
+
+			//checking if file equals -u flag  	
+
+			for(String arg:args)
+			{
+				if(args.equals("-")) 
+				{
+					if (args[0].equals("-u"))
+
+					{
+						inputFile = new File(args[1]);
+						outputFile = new File(args[2]);
+					}
+
+					else 
+					{
+						inputFile = new File(args[0]); //rearranging files to be in correct order
+						outputFile = new File(args[1]);
+					}
+
+					//Creating Scanner Input
+					
+					Scanner input  = new Scanner(inputFile);
+
+					//creating Scanner output 
+					outputFile.createNewFile();  
+					FileWriter Writer new FileWriter(outputFile);
+
+					//Reading Scanner Input 
+					While(input.hasNextLine()){ 
+						String Firstname = input.next();
+						String Surname = input.next();
+						String date = input.next();
+
+						if(args[0].equals ("-u"))
+						{
+							Firstname.toUpperCase(); //Puts name in all caps 
+							Surname.toUpperCase();// puts Last name in all caps
+
+						}
+						else {
+							Firstname.toLowerCase();  //reverts it back to lower case
+							Surname.toLowerCase();
+						}
+
+						//Setting Pattern of Date of Birth
+						String datePattern = "MM-dd-yyyy"; //Configuring Date (//Month/Day/Year)
+						SimpleDateFormat InputDate = new SimpleDateFormat(datePattern); //Input File
+						SimpleDateFormat outputDate = new SimpleDateFormat(datePattern); //Output File
+
+						//Configuring Date of Birth
+
+						try {
+							String Date_of_birth = InputDate.format(new Date());//Output Data
+							String date = outputDate.format(new Date());//Output Data
+							System.out.println(Firstname + Surname + outputDate);
+
+							//Catching any errors 
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						writer.close();
+						
+			} // main
+
+		   }// FilesInOut
--- FormatNamesm.java
+++ FormatNamesm.java
@@ -6,7 +6,11 @@ import javax.swing.*;
 import java.lang.Number;
 import java.io.File;
 import java.util.Scanner;
-import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
 
 
 /**
@@ -16,42 +20,99 @@ import java.io.File;
  */
 public class FormatNamesm {
 
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+	public static void main(String[] args) {
+		try {
+			File inputFile;
+			File outputFile;
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
+			//checking if file equals -u flag  	
 
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
+			for(String arg:args)
+			{
+				if(args.equals("-")) 
+				{
+					if (args[0].equals("-u"))
 
-        // Finally, add code to read the filenames as arguments from the command line.
-    	try {
-            System.out.print("Enter the file name with extension : ");
+					{
+						inputFile = new File(args[1]);
+						outputFile = new File(args[2]);
+					}
 
-            Scanner input = new Scanner(System.in);
+					else 
+					{
+						inputFile = new File(args[0]); //rearranging files to be in correct order
+						outputFile = new File(args[1]);
+					}
 
-            File file = new File(input.nextLine());
+					//Creating Scanner Input
 
-            input = new Scanner(file);
+					Scanner input  = new Scanner(inputFile);
 
-
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println(line);
-            }
-            input.close();
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+					//creating Scanner output 
+					outputFile.createNewFile();  
+					FileWriter Writer new FileWriter(outputFile);
 
 
-    } // main
+					//Reading Scanner Input 
+					While(input.hasNextLine()){ 
 
- // FilesInOut
+						String Firstname = input.next();
+						String Surname = input.next();
+						String MiddleName = "";
+
+
+						if(args[0].equals ("-u"))
+						{
+							Firstname.toUpperCase(); //Puts name in all caps 
+							Surname.toUpperCase();// puts Last name in all caps
+							MiddleName.toUpperCase();// Puts Middle name in all caps if there
+						}
+						else {
+							Firstname.toLowerCase();  //reverts it back to lower case
+							Surname.toLowerCase();
+						}
+
+						//Setting Pattern of Date of Birth
+						String datePattern = "MM-dd-yyyy"; //Configuring Date (//Month/Day/Year)
+						SimpleDateFormat InputDate = new SimpleDateFormat(datePattern); //Input File
+						SimpleDateFormat outputDate = new SimpleDateFormat(datePattern); //Output File
+
+						//Configuring Date of Birth
+
+						try {
+							String Date_of_birth = InputDate.format(new Date());//Output Data
+							String date = outputDate.format(new Date());//Output Data
+
+							//checking if person has a middle name 
+
+							//checking if Middle name is there
+							int firstSpace = Firstname.indexOf(" ");
+							Firstname =Firstname.substring(0,firstSpace);
+
+							int lastSpace = Surname.lastIndexOf(" ", firstSpace);
+							String middleName = MiddleName.substring(firstSpace,lastSpace);
+
+							if(lastSpace<0) {
+								Surname =Surname.substring(lastSpace + 1);
+								System.out.println(Firstname + ","+ Surname);
+							}
+
+							else {
+								MiddleName = MiddleName.substring(firstSpace, lastSpace);
+								Surname = Surname.substring(lastSpace + 1);
+								System.out.println(Firstname + "," + MiddleName.charAt(1) + "," + Surname + date );
+							}
+
+
+
+							//Catching any errors 
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						writer.close();
+
+
+
+					} // main
+
+					// FilesInOut
